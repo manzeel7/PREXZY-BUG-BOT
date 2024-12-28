4887365acf5790cf4f630748b53370cc31a48acb
manzeel7 <mouhamedmandiang86@gmail.com>
2024-12-28 07:52:12 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,12 +3,12 @@ const chalk = require('chalk')
 
 //contact details
 global.ownernomer = "263717750825"
-global.ownername = "Precious Ayomide👑"
+global.ownername = "Prexzy tech"
 global.ytname = "YT: Prexzyvilla"
 global.socialm = "GitHub: Prexzybooster"
 global.location = "Nigeria, Osun, Osogbo"
 
-global.ownernumber = '263717750825'  //creator number
+global.ownernumber = '221706389718'  //creator number
 global.ownername = 'Precious Ayomide👑' //owner name
 global.botname = 'PREXZY ᵇᵘᵍ ᵇᵒᵗ' //name of the bot
 
